6ab82fd526bf3c688762ca53625a6a6805fe33fb
user.name:"nongkeen <user.email :"taskeen073@gmail.com>
2021-09-23 01:53:17 +0700

    o

--- lib/create_account.dart
+++ lib/create_account.dart
@@ -11,34 +11,7 @@ class CreateAccount extends StatefulWidget {
 
 class _CreateAccountState extends State<CreateAccount> {
   String? typeUser;
-  Row buildName(double size) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Container(
-          margin: EdgeInsets.only(top: 16),
-          width: size * 0.6,
-          child: TextFormField(
-            decoration: InputDecoration(
-              labelStyle: MyConstant().h3(),
-              labelText: 'User: ',
-              prefixIcon:
-                  Icon(Icons.account_circle_outlined, color: MyConstant.dark),
-              enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: MyConstant.dark,
-                  ),
-                  borderRadius: BorderRadius.circular(30)),
-              focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: MyConstant.yl),
-                  borderRadius: BorderRadius.circular(30)),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
+  
   @override
   Widget build(BuildContext context) {
     double size = MediaQuery.of(context).size.width;
@@ -113,4 +86,32 @@ class _CreateAccountState extends State<CreateAccount> {
       ),
     );
   }
+  Row buildName(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.only(top: 16),
+          width: size * 0.6,
+          child: TextFormField(
+            decoration: InputDecoration(
+              labelStyle: MyConstant().h3(),
+              labelText: 'User: ',
+              prefixIcon:
+                  Icon(Icons.account_circle_outlined, color: MyConstant.dark),
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: MyConstant.dark,
+                  ),
+                  borderRadius: BorderRadius.circular(30)),
+              focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: MyConstant.yl),
+                  borderRadius: BorderRadius.circular(30)),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
 }
