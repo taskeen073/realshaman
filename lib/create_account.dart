63e1f1c82aa22f3925ff1eb8e596e62ad2f96370
user.name:"nongkeen <user.email :"taskeen073@gmail.com>
2021-09-28 23:09:26 +0700

    --- Auto-Git Commit ---

--- lib/create_account.dart
+++ lib/create_account.dart
@@ -15,6 +15,7 @@ class _CreateAccountState extends State<CreateAccount> {
   String? typeUser;
   bool statusRedEye = true;
   String? typeSex;
+
   String? name, lastname, username, password, choosetype;
   final fromKey = GlobalKey<FormState>();
   @override
